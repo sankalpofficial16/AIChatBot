737788d91e3cbd918bc23681d97904a5a8c51931
Sankalp <53269004+sankalpofficial16@users.noreply.github.com>
2021-05-07 00:17:14 +0530

    Update aichatbot.py
    Comments Added

--- aichatbot.py
+++ aichatbot.py
@@ -15,6 +15,8 @@ pip install chatterbot_corpus
 
 pip install functions
 
+# function to create the chatbot
+# we have the read_only to false so the chatbot learns from the user input as
 def create_bot(name):
   from chatterbot import ChatBot
   Bot = ChatBot(name=name,
@@ -23,16 +25,24 @@ def create_bot(name):
                 storage_adapter="chatterbot.storage.SQLStorageAdapter")
   return Bot
 
+# function to train the bot with a variety of topics
+# the language we have chosen is english
+# we can train the bot for other languages as well
 def train_all_data(Bot):
   from chatterbot.trainers import ChatterBotCorpusTrainer
   corpus_trainer = ChatterBotCorpusTrainer(Bot)
   corpus_trainer.train("chatterbot.corpus.english")
 
+# function to train the bot with custom data
+# it uses ListTrainer to train data from lists 
 def custom_train(Bot,conversation):
   from chatterbot.trainers import ListTrainer
   trainer=ListTrainer(Bot)
   trainer.train(conversation)
 
+# function to start and take responses from the chatbot
+# the chatbot stays running unless a word is typed from the bye_list 
+def start_chatbot(Bot):
 def start_chatbot(Bot):
   print('\033c')
   print("Hello, I am Joy. How can I help you?")
